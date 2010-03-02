c0bcc9210907fc9b7a39d6dcc059e52eb4009aff
Nic Bell <me@nicbell.net>
2010-03-02 10:22:26 +0000

    Extending Array not working in IE6 but implementing function into Array does.

--- Source/Collections.js
+++ Source/Collections.js
@@ -1,6 +1,6 @@
 /*
 ---
-description:  Generics.Collection and Generics Mixin for Classes.
+description:  Generics.Collection extends array to only accept items of a specific type.
 
 license: MIT-style
 
@@ -9,40 +9,56 @@ authors:
 - Kevin Chapelier
 
 requires:
- core/1.2.4:
-  - Class
-  - Array
+core/1.2.4:
+- Class
+- Array
 
-provides: [Generics, Generics.Collection]
+provides: [Generics.Collection]
 
 ...
 */
 
 var Generics = {
-	Collection: new Class({
-		Extends: Array,
-		initialize: function(type) {
-			this.type = type;
-		},
-		push: function(item) {
-			if($type(item) == 'object') {
-				if (item instanceof this.type) {
-					if(!this.contains(item)) {
-						this.parent(item);
-					}
-				}
-				else {
-					alert('Type exception: item of incorrect type.');
-				}
-			}
-			else {
-				alert('Type exception: item is not an object.');
-			}
-		},
-		/* for compatiblity with 0.5 */
-		add: function(item) { this.push(item); },
-		remove: function(item) { this.erase(item); }
-	})
+
+    Collection: new Class({
+        Extends: Array,
+        initialize: function(type) {
+            this.type = type;
+        },
+        push: function(item) {
+            if ($type(item) == 'object') {
+                if (item instanceof this.type) this.parent(item);
+                else alert('Type exception: item of incorrect type.');
+            }
+            else {
+                alert('Type exception: item is not an object.');
+            }
+        },
+        /* for compatiblity with 0.5 and because every other language uses add & remove on collections */
+        add: function(item) { this.push(item); },
+        remove: function(item) { this.erase(item); }
+    }),
+
+    FakeCollection: function(type) {
+        var Collection = Array;
+        Collection.implement({
+            type: type,
+            add: function(item) {
+                if ($type(item) == 'object') {
+                    if (item instanceof this.type)
+                        this.push(item);
+                    else
+                        alert('Type exception: item of incorrect type.');
+                }
+                else {
+                    alert('Type exception: item is not an object.');
+                }
+            },
+            /* for compatiblity with 0.5 and because every other language uses add & remove on collections */
+            remove: function(item) { this.erase(item); }
+        });
+        return new Collection();
+    }
 }
 /* Ends */
 
